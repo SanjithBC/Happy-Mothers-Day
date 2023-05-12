da0ee18d35522d18ed80e1465f5eca6c04428d59
SanjithBC <111051325+SanjithBC@users.noreply.github.com>
2023-05-11 20:25:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function changeImageOver() {
+    document.getElementById("img1").src="happy mothers day 2023.gif";
+}
+
+function changeImageLeave() {
+    document.getElementById("img1").src="happy mothers day.jpg";
+}
+
+function changeImageOver2() {
+    document.getElementById("img2").src="mothers day.png";
+}
+
+function changeImageLeave2() {
+    document.getElementById("img2").src="mom.jpg";
+}
+
+function changeImageOver3() {
+    document.getElementById("img3").src="earth mom gif.gif";
+}
+
+function changeImageLeave3() {
+    document.getElementById("img3").src="mom gif.gif";
+}
